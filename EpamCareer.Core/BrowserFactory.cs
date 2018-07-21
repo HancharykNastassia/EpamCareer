f225a8b86301bad3a64e3f40837acc0e379e3702
HancharykNastassia <goncharik.anastasia@mail.ru>
2018-07-21 17:16:04 +0300

    Added App.config and Browser Factory

---
+++ EpamCareer.Core/BrowserFactory.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace EpamCareer.Core
+{
+  public enum BrowserType
+  {
+    Firefox,
+    Chrome,
+  }
+
+  public static class BrowserFactory
+  {
+     public static IWebDriver GetDriver(BrowserType type, int timeoutSec)
+    {
+      IWebDriver driver;
+      switch (type)
+      {
+        case BrowserType.Firefox:
+          {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(timeoutSec);
+            break;
+          }
+        case BrowserType.Chrome:
+          {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(timeoutSec);
+            break;
+          }
+        default:
+          {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(timeoutSec);
+            break;
+          }
+      }
+      return driver;
+    }
+  }
+}
---
+++ EpamCareer.Core/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace EpamCareer.Core
+{
+  public static class Configuration
+  {
+    public static string Browser => GetConfirurationParameter("Browser", "Firefox");
+
+    public static string Timeout => GetConfirurationParameter("Timeout", "30");
+
+    public static string GetConfirurationParameter(string parameter, string defaultValue)
+    {
+      return ConfigurationManager.AppSettings[parameter] ?? defaultValue;
+    }
+  }
+}
